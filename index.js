f208aa74cac0fbd927e87bd29162c1514b9a65a8
Gabriel Rezende <g@brielrezen.de>
2020-10-11 12:37:08 -0300

    Adicionei uma função que mostra as notas

---
+++ index.js
@@ -0,0 +1,24 @@
+// hEADLINE, bODY, dATA
+
+let notas = [
+  {
+    h: 'Primeira nota',
+    b: 'Aqui é a primeira nota'
+  },
+  {
+    h: 'Segunda nota',
+    b: 'Texto da segunda nota'
+  }
+]
+
+const mostraNotas = () => {
+  for (let obj of notas){ // For of para arrays e strings
+    console.log('-----');
+    for (let prop in obj) { // For in para objetos
+      console.log(obj[prop]);
+    }
+  }
+  console.log('-----');
+}
+
+mostraNotas();
