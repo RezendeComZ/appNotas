28d3365aaaff2db1c9796fc70b75056ebb5f5c94
Gabriel Rezende <g@brielrezen.de>
2020-10-18 17:55:06 -0300

    esse notasSalvas.js ainda não funciona

---
+++ notas/notasSalvas.js
@@ -0,0 +1,48 @@
+// Ainda não funfa
+
+let notas = [
+  {
+    id: 1,
+    h: 'Primeira nota',
+    b: 'Aqui body da primeira',
+    pin: false
+  },
+  {
+    id: 2,
+    h: 'Segunda nota',
+    b: 'Body da segunda nota',
+    pin: false
+  },
+  {
+    id: 3,
+    h: 'Ouvir música',
+    b: 'No player do spotify',
+    pin: true
+  },
+  {
+    id: 5,
+    h: 'Assistir Matrix',
+    b: 'Pegar o bluray',
+    pin: false
+  },
+  {
+    id: 6,
+    h: 'Ver série',
+    b: 'Pelo app da Netflix',
+    pin: false
+  },
+  {
+    id: 7,
+    h: 'Comprar livro',
+    b: 'Clean code',
+    pin: true
+  },
+  {
+    id: 8,
+    h: 'Aprender a usar JSON',
+    b: 'e usar nesse mesmo projeto',
+    pin: false
+  }
+]
+
+module.exports = notas
